9778b4ac043f70d00875157f168a9f25a1ce7b5c
lanfon72 <lanfon72@gmail.com>
2015-03-18 08:55:42 -0400

    fix newline bug.

--- log.py
+++ log.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #coding:utf8
 import sys
-import requests, json, re
+import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
@@ -12,10 +12,10 @@ url = "http://ivod.ly.gov.tw/Play/VOD/"
 columns = [ "流水號", "主辦單位", "屆期", "委員", "發言時間", "影片長度", "會議時間", "會議簡介", "相關文檔" ]
 sv = sys.argv[1]
 f = open(sv,'wb+')
-f.write(','.join(columns) + "\n")
+f.write( ','.join(columns)+'\n' )
 print "===save on {}, start from {} to {}".format(sys.argv[1], sys.argv[2], sys.argv[3])
 for uid in range(int(sys.argv[2]), int(sys.argv[3])):
-    sleep(1)
+    sleep(0.1)
     html = requests.get(url+str(uid))
     html.encoding = "utf8"
     soup = BeautifulSoup(html.text)
@@ -31,11 +31,11 @@ for uid in range(int(sys.argv[2]), int(sys.argv[3])):
         continue
     ptr.append( strs[1] ) #屆期
     ptr += [_.split(u"：")[1] for _ in strs[2:6]]
-    ptr.append( strs[6].split(u"會議簡介：")[1].replace("\t",'').strip() ) #會議簡介
+    ptr.append( strs[6].split(u"會議簡介：")[1].replace("\t",'').replace('\n','\\n').replace('\r','\\r').strip() ) #會議簡介
     href = content.find("a",href=True)
     if href:
         ptr.append( href["href"] ) #相關文檔
-
-    f.write(",".join(ptr).encode('utf8') + "\n")
+    
+    f.write(','.join(ptr).encode('utf8')+'\n')
     ptr=[]
 f.close()
